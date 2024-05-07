80eb30b427110a2d2abeadcf2dfdae3733d3b422
Erik Hoversten <ehoversten@gmail.com>
2024-05-06 22:00:24 -0400

    updated EventList layout and styling

--- client/src/components/Events/Events.jsx
+++ client/src/components/Events/Events.jsx
@@ -26,10 +26,19 @@ function Events({ events, removeEvent, newBooking, setChoice }) {
     <div className='bg-sky-800 divide-y divide-slate-700'>
       { events.length == 0 ? <h3>No Current Events</h3> :
         events?.length && events?.map(event => (
-        <div className="event-card p-5" key={event._id} onClick={() => setChoice(event)}>
-          <h3 className="event-title">Title: {event.title}</h3>
-          <h4 className="event-date">Date: {event.date}</h4>
-          <h4 className="event-time">Time: {event.time}</h4>
+        <div className="event-card p-5 flex justify-between" key={event._id} onClick={() => setChoice(event)}>
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm mr-5 flex-column content-center rounded-full bg-sky-900">
+            <img
+              className="mx-auto h-10 w-auto align-middle"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Your Company"
+            />
+          </div>
+          <div className="event-info text-left flex-1">
+            <h3 className="event-title"><span className='font-bold'>Event: </span> {event.title}</h3>
+            <h4 className="event-date"><span className='font-bold'>Date: </span> {event.date}</h4>
+            <h4 className="event-time"><span className='font-bold'>Time: </span> {event.time}</h4>
+          </div>
         </div>
       ))}
     </div>
