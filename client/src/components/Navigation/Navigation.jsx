e998e8f78278924cd9093ebfb24b592196561b24
Erik Hoversten <ehoversten@gmail.com>
2024-05-10 21:28:20 -0400

    added Navigation Component collapsible functionality

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState, useRef, createRef } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navigation() {
+
+  const navRef = useRef()
+
+  const showNav = () => {
+    navRef.current.classList.toggle('hidden')
+  }
 
   const handleLogout = () => {
     if(localStorage.getItem('id_token')) {
@@ -11,27 +17,59 @@ function Navigation() {
   }
 
   return (
-    <div className='text-center mx-auto bg-sky-600 flex-col divide-y divide-slate-700 w-full'>
-      <div className="link-row p-3">
-        <Link to='/login'>Login</Link>
+    <div className='text-center mx-auto bg-sky-600 flex-col divide-y divide-slate-700 w-full min-h-8 relative'>
+      {/* <!-- Hamburger Icon --> */}
+      <svg
+        xmlns="<http://www.w3.org/2000/svg>"
+        id="menu-button"
+        class="h-8 w-8 cursor-pointer md:hidden block end-0"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        onClick={showNav}
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 12h16M4 18h16"
+        />
+      </svg>
+      <div class="hidden w-full md:flex md:items-center md:w-auto md:justify-end" id="menu" ref={navRef}>
+        <ul
+          class="
+            text-base text-gray-700
+            md:flex
+            md:justify-between
+            md:pt-0
+            divide-y divide-slate-700"
+        >
+          <div className="link-row p-3">
+            <Link to='/'>Home</Link>
+          </div>
+          <div className="link-row p-3">
+            <Link to='/events'>Events</Link>
+          </div>
+
+          <div className="link-row p-3">
+            <Link to='/bookings'>Bookings</Link>
+          </div>
+          <div className="link-row p-3">
+            <Link to='/login'>Login</Link>
+          </div>
+          <div className="link-row p-3">
+            <Link to='/signup'>Register</Link>
+          </div>
+          <div className="link-row p-3">
+            <a onClick={handleLogout} className='logout-btn'>Logout</a>
+          </div>
+          
+        </ul>
       </div>
-      <div className="link-row p-3">
-        <Link to='/signup'>Register</Link>
-      </div>
-      <div className="link-row p-3">
-        <Link to='/'>Home</Link>
-      </div>
-      <div className="link-row p-3">
-        <Link to='/events'>Events</Link>
-      </div>
-      <div className="link-row p-3">
-        <Link to='/bookings'>Bookings</Link>
-      </div>
-      <div className="link-row p-3">
-        <a onClick={handleLogout} className='logout-btn'>Logout</a>
-      </div>
+      
     </div>
   )
 }
+
 
 export default Navigation
